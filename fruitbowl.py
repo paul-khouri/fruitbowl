1ebaf8d19462e42a976dfe2baeaf6d5ea089d521
Paul Khouri <paul.khouri@marsden.school.nz>
2020-04-09 22:02:16 +1200

    Test change new one more arrows

--- fruitbowl.py
+++ fruitbowl.py
@@ -105,8 +105,8 @@ def eat_fruit(F):
         my_string = "{:<5}{:10}".format(i, F[i][0])
         print(my_string)
     print("-" * 60)
-    option = get_integer("Choose option number of fruit to eat", 0, len(F))
-    my_amount = get_integer("How many {} would you like to eat?".format(F[option][0]), 0, F[option][1]-95)
+    option = get_integer("Choose option number of fruit to eat -- > ", 0, len(F))
+    my_amount = get_integer("How many {} would you like to eat -- > ?".format(F[option][0]), 0, F[option][1]-95)
     F[option][1]=F[option][1]-my_amount
     response_string = "You now have {} {} in the fruitbowl".format(F[option][1], F[option][0])
     print(response_string)
