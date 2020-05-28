56193a20e340d131a9d87baae6fd6d7aed96531c
Khouri <khouripa@localadmins-MBP.smc.local>
2020-05-28 15:06:03 +1200

    Onto new laptop

---
+++ class_fruitbowl/fruit_testing.py
@@ -0,0 +1,7 @@
+
+
+
+
+def review_fruit():
+    print("review fruit function")
+    return None
---
+++ class_fruitbowl/main_fruit.py
@@ -0,0 +1,14 @@
+
+
+
+def review_fruit():
+    print("review fruit function")
+    return None
+
+
+def main():
+    print("Main function")
+
+
+if __name__ == "__main__":
+    main()
