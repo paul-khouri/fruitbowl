7ec344e4c1ab3d2aa9e5479ea6836e7b482c7012
paul-khouri <paul.khouri@marsden.school.nz>
2021-05-16 10:49:49 +1200

    Update before 2021 fruibowl start

--- class_fruitbowl/fruit_testing.py
+++ class_fruitbowl/fruit_testing.py
@@ -3,15 +3,26 @@
 def add_to_fruit_bowl():
     return None
 
-def review_fruit():
-    print("review fruit function")
+def review_fruit(l):
+    for x in l:
+        output = "{:<10} -- {:>4}".format(x[0], x[1])
+        print(output)
     return None
 
 
 
 
 def main():
-    review_fruit()
+
+
+    fruit_list = [
+        ["apples", 2],
+        ["pears", 0],
+        ["quinces", 3],
+        ["lemons", 7]
+    ]
+
+    review_fruit(fruit_list)
     return None
 
 main()
